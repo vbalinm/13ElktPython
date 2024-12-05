4eb14c58c7934ab0c4b088267f8acc0d457f614b
vbalintm <98655410+vbalinm@users.noreply.github.com>
2024-12-05 17:42:41 +0100

    Delete madlib.py

--- madlib.py
+++
@@ -1,8 +0,0 @@
-melleknev = input("Adj meg egy melléknevet: ")
-ige1 = input("Adj meg egy igét: ")
-ige2 = input("Adj meg egy második igét: ")
-hires_ember = input("Adj meg egy híres ember nevét: ")
-
-madlibs = f"Nagyon szeretek programozni, mert {melleknev} tőle. {ige1}, {ige2}, de nem repülök. Úgy nézel ki, mint {hires_ember}!"
-
-print(madlibs)
